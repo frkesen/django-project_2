2440586a98b20d0465fd8f1d29f2eb1e0407b083
Faruk <felixfranko66@gmail.com>
2020-12-26 18:32:36 +0300

    added home template

--- src/fscohort/models.py
+++ src/fscohort/models.py
@@ -4,3 +4,6 @@ class Student(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     number = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name +" "+ self.last_name
--- src/fscohort/views.py
+++ src/fscohort/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_view(request):
+    # print(request.GET.get("q"))
+    # print(request.COOKIES)
+    # print(request.user)
+    # print(request.path)
+    my_context = {
+        "title": "clarusway",
+        "dict_1": {"djang": "best framework"},
+        "my_list": [2, 3, 4, 5],
+    }
 
-    return HttpResponse("Hi, this is fscohort Home page.")
+    return render(request, "fscohort/home.html", my_context)
 
 def about_view(request):
     return HttpResponse("Hi ,this is fscohort about page.")
